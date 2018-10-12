8ff84c51236884787d11b17a9e76db75a7be6c5e
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-10-13 02:37:13 +0800

    Revert "change keys"
    This reverts commit 7b5a485fcc6de71fbf283d582ad60b997afe0990.

--- contracts/contracts/SpokkzCoin.py
+++ contracts/contracts/SpokkzCoin.py
@@ -1,4 +1,4 @@
-from boa.interop.System.Runtime import *
+
 from boa.interop.System.Storage import *
 from boa.builtins import *
 
@@ -12,10 +12,7 @@ TOKEN_SYMBOL = 'SPKZ'
 ################################################################################
 # TOKEN INFO CONSTANTS
 
-DEPLOYER = bytearray(
-    b'\x15\xd7\x1d\x26\xc7\x22\x84\xc2\xf6\x46'
-    b'\xff\x4f\xde\xfd\xae\xdc\x65\xdc\xfe\x8d'
-)
+DEPLOYER = "AHmMXuCARpydrg8erE9FeSsoufgbYgbf1T"
 INIT_SUPPLY = 1000000000
 TOKEN_DECIMALS = 8
 FACTOR = 100000000
@@ -25,7 +22,7 @@ FACTOR = 100000000
 # Belows are storage key for some variable token information.
 
 OWNER_KEY = '___OWNER'
-SPKZ_SUPPLY_KEY = '__SUPPLY'
+MZK_SUPPLY_KEY = '__SUPPLY'
 
 
 ################################################################################
@@ -83,15 +80,15 @@ def Deploy():
     """
     Constructor of this contract. Only deployer hard-coded can call this function
     and cannot call this function after called once.
+
     Followings are initialization list for this token
     1. Transfer the owner to the deployer. (Owner can mint and burn the token)
     2. Supply initial coin to the deployer.
     """
-
     ctx = GetContext()
 
     # Require(CheckWitness(DEPLOYER))         # only can be initialized by deployer
-    # Require(not Get(ctx, 'DEPLOYED'))       # only can deploy once
+    Require(not Get(ctx, 'DEPLOYED'))       # only can deploy once
 
     # disable to deploy again
     Put(ctx, 'DEPLOYED', 1)
@@ -101,7 +98,7 @@ def Deploy():
     Put(ctx, OWNER_KEY, DEPLOYER)
 
     # supply the coin. All coin will be belong to deployer.
-    Put(ctx, SPKZ_SUPPLY_KEY, INIT_SUPPLY * FACTOR)
+    Put(ctx, MZK_SUPPLY_KEY, INIT_SUPPLY * FACTOR)
     Put(ctx, concat(OWN_PREFIX, DEPLOYER), INIT_SUPPLY * FACTOR)
 
     return True
@@ -109,7 +106,7 @@ def Deploy():
 
 def TotalSupply():
     """
-    Gets the total supply for SPKZ token. The total supply can be changed by
+    Gets the total supply for MZK token. The total supply can be changed by
     owner's invoking function calls for minting and burning.
     """
     return _totalSupply(GetContext())
@@ -117,7 +114,8 @@ def TotalSupply():
 
 def BalanceOf(account):
     """
-    Gets the SPKZ token balance of an account.
+    Gets the MZK token balance of an account.
+
     :param account: account
     """
     return _balanceOf(GetContext(), account)
@@ -127,9 +125,10 @@ def Transfer(_from, _to, _value):
     """
     Sends the amount of tokens from address `from` to address `to`. The parameter
     `from` must be the invoker.
+
     :param _from: invoker address.
     :param _to: receiver address.
-    :param _value: SPKZ amount.
+    :param _value: MZK amount.
     """
     RequireWitness(_from)           # from address validation
     return _transfer(GetContext(), _from, _to, _value)
@@ -139,21 +138,23 @@ def TransferFrom(_originator, _from, _to, _amount):
     """
     Transfers the amount of tokens in `from` address to `to` address by invoker.
     Only approved amount can be sent.
+
     :param _originator: invoker address.
     :param _from: address for withdrawing.
     :param _to: address to receive.
-    :param _amount: SPKZ amount.
+    :param _amount: MZK amount.
     """
     return _transferFrom(GetContext(), _originator, _from, _to, _amount)
 
 
 def Approve(_from, _to, _amount):
     """
-    Approves `to` address to withdraw SPKZ token from the invoker's address. It
+    Approves `to` address to withdraw MZK token from the invoker's address. It
     overwrites the previous approval value.
+
     :param _from: invoker address.
     :param _to: address to approve.
-    :param _amount: SPKZ amount to approve.
+    :param _amount: MZK amount to approve.
     """
     RequireWitness(_from)       # only the token owner can approve
     return _approve(GetContext(), _from, _to, _amount)
@@ -161,8 +162,8 @@ def Approve(_from, _to, _amount):
 
 def Burn(_amount):
     """
-    Burns the amount of SPKZ token from the owner's address.
-    :param _amount: SPKZ amount to burn.
+    Burns the amount of MZK token from the owner's address.
+    :param _amount: MZK amount to burn.
     """
     ctx = GetContext()
     _onlyOwner(ctx)                             # only owner can burn the token
@@ -171,7 +172,7 @@ def Burn(_amount):
 
 def Mint(_to, _amount):
     """
-    Mints the amount of SPKZ token.
+    Mints the amount of MZK token.
     :param _to: address to receive token.
     :param _amount: the amount to mint.
     """
@@ -271,7 +272,7 @@ def _burn(_context, _account, _amount):
     total_supply = uSub(total_supply, _amount)
 
     SafePut(_context, concat(OWN_PREFIX, _account), account_val)
-    SafePut(_context, SPKZ_SUPPLY_KEY, total_supply)
+    SafePut(_context, MZK_SUPPLY_KEY, total_supply)
     return True
 
 
@@ -286,7 +287,7 @@ def _mint(_context, _to, _amount):
     total_supply += _amount
     to_val += _amount
 
-    SafePut(_context, SPKZ_SUPPLY_KEY, total_supply)
+    SafePut(_context, MZK_SUPPLY_KEY, total_supply)
     SafePut(_context, concat(OWN_PREFIX, _to), to_val)
     return True
 
@@ -315,7 +316,7 @@ def _accountValue(_context, _account):
 
 
 def _totalSupply(_context):
-    return Get(_context, SPKZ_SUPPLY_KEY)
+    return Get(_context, MZK_SUPPLY_KEY)
 
 
 def _allowance(_context, _from, _to):
--- utils/deployer.ts
+++ utils/deployer.ts
@@ -29,7 +29,7 @@ export class Deployer {
       '',
       true,
       '0',
-      '20600000',
+      '20400000',
       payer
     );
 
