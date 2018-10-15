a64539aac6fd52ae1639f5a44da3b7cfc4540ce2
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-10-15 22:47:46 +0800

    change deployment

--- migrations/01_deploy_token.ts
+++ migrations/01_deploy_token.ts
@@ -1,11 +1,7 @@
 import { Deployer } from "../utils/deployer";
-import { utils } from 'ontology-ts-sdk';
 
 module.exports = async (deployer: Deployer) => {
-  const contract = await deployer.deploy('TestContract', 'Deploy');
-  contract.deployed()
-    .then(async (aa) => {
-      console.log('contract hash', contract.codeHash);
-      console.log(await deployer.client.getStorage(contract.codeHash, utils.str2hexstr('test')));
-    });
+  const contract = await deployer.deploy('SpokkzCoin', 'Deploy');
+  await contract.deployed();
+  console.log('contract hash :', contract.codeHash);
 };
