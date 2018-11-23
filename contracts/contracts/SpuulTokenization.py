d1d4b3c255f4cf193045eab903d0c910d58ea8a8
Jimbo Cortes <jimbo.cortes@gmail.com>
2018-11-24 03:10:31 +0800

    able to call pay

--- contracts/contracts/SpuulTokenization.py
+++ contracts/contracts/SpuulTokenization.py
@@ -1,16 +1,24 @@
 from boa.interop.System.ExecutionEngine import GetExecutingScriptHash
 from boa.interop.System.Runtime import *
-from boa.interop.System.App import RegisterAppCall
+from boa.interop.System.App import RegisterAppCall, DynamicAppCall
 from libs.SafeCheck import *
 
+SpokkzOEP4Contract = RegisterAppCall('5682464183f3972abbf72105a8e2a89ac5083a90', 'operation', 'args')
+
 DEPLOYER = ToScriptHash('Ac725LuR7wo481zvNmc9jerqCzoCArQjtw')
+
 OWNER_KEY = '___OWNER'
+
+PAYMENT_PREFIX = '_____pay'
 
 ctx = GetContext()
 
 def main(operation, args):
     if operation == 'deploy':
         return deploy()
+    if operation == 'pay':
+        if len(args) == 4:
+            return pay(args[0],args[1],args[2],args[3])
     if operation == 'transferOwnership':
         if len(args) == 1:
             return transferOwnership(args[0])
@@ -41,6 +49,11 @@ def deploy():
 
     return True
 
+
+def pay(_originator, _from, _amount, _order_id):
+    Require(_pay(_originator, _from, _amount, _order_id))
+    return True
+
 def transferOwnership(_account):
     """
     Transfers the ownership of this contract to other.
@@ -49,6 +62,9 @@ def transferOwnership(_account):
     _onlyOwner()
     Require(_transferOwnership(_account))
     return True
+
+def CallOep4Contract(operation, params):
+    return SpokkzOEP4Contract(operation, params)
 
 
 ################################################################################
@@ -62,6 +78,17 @@ def _transferOwnership(_account):
     Put(ctx, OWNER_KEY, _account)
     return True
 
+def _pay(_originator, _from, _amount, _order_id):
+    RequireWitness(_originator)
+    RequireScriptHash(_from)
+
+    Require(_amount > 0)
+
+    CallOep4Contract('transferFrom', _originator, _from, GetExecutingScriptHash(), _amount)
+
+    payment_key = concat(PAYMENT_PREFIX, _order_id)
+    Put(ctx, payment_key, _amount)
+    return True
 
 ################################################################################
 # modifiers
